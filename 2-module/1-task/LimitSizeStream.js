521e596ed672161acfb9298e03f60bc21883fd80
Anton Lantukh <alantukh@yandex-team.ru>
2020-03-22 11:49:46 +0300

    2-module-1

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -1,13 +1,20 @@
-const stream = require('stream');
+const {Transform} = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
-class LimitSizeStream extends stream.Transform {
-  constructor(options) {
+class LimitSizeStream extends Transform {
+  constructor({limit, ...options}) {
     super(options);
+    this.limit = limit;
+    this.aggregatedSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.aggregatedSize += chunk.length;
+    if (this.aggregatedSize <= this.limit) {
+      callback(null, chunk);
+    } else {
+      callback(new LimitExceededError());
+    }
   }
 }
 
